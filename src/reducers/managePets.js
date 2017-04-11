1bcc15b2c96543d1a00cf0abff2dffc90d8f8668
mwei2509 <m2dl89@gmail.com>
2017-04-11 18:18:27 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,26 @@
 export let state;
+import jsdom from 'jsdom'
 
-
-export function managePets(){
+export function managePets(state={pets: []}, action){
+  switch(action.type){
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      return {pets: state.pets.filter((o)=>{return o.id !== action.id})}
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  global.document=jsdom.jsdom('<html><body><div id="container"></div></body></html>')
+  let container=document.getElementById('container')
+  if (state.pets[0]){
+    container.innerHTML = `<ul><li>${state.pets[0].name}</li></ul>`
+  }
 }
