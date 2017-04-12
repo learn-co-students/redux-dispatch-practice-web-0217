0a381d1f7fb8b9f0135b7975a22353c4891448ba
hwalborn <hwalborn@gmail.com>
2017-04-11 20:22:32 -0400

    pets

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,32 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case "ADD_PET":
+      let addedPet = {pets: [...state.pets, action.pet]}
+      return addedPet
+    case "REMOVE_PET":
+      let removedPet = {pets: state.pets.filter(pet => pet.id !== action.id)}
+      return removedPet
+    default:
+      return state
+  }
+  if(action.type === "ADD_PET"){
+
+  }else if(action.ty)
+  return state
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let petsLi = []
+  state.pets.map((pet) => {
+    petsLi.push(`<ul><li>${pet.name}</li></ul>`)
+  })
+  document.getElementById('container').innerHTML = petsLi
 }
