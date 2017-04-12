ec6ea5cc4b449c75a97b30276769afd450c822d1
chasms <chasms@chas.ms>
2017-04-12 14:01:42 -0400

    3 more tests

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,10 +1,19 @@
 export let state;
 
 
-export function managePets(state, action){
+export function managePets(state = { pets: [] }, action){
   switch (action.type) {
-    case '@@INIT':
-
+    case 'ADD_PET':
+      if (action.pet.name && action.pet.species && action.pet.id) {
+        return {...state, pets: state.pets.concat(action.pet)}
+      }
+      break;
+    case 'REMOVE_PET':
+      if (action.id) {
+        return {...state, pets: state.pets.filter( (obj) => {
+          return obj.id !== action.id
+        })}
+      }
       break;
     default: return state
 
