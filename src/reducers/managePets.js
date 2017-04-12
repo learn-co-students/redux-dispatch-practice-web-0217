998a8fc236548cb57dbf8a86163a8ef448ff598b
fattyduck <dison.ruan@yahoo.com>
2017-04-12 08:43:00 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,22 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets :[]}, action){
+  switch (action.type) {
+    case "ADD_PET":
+      return {pets: state.pets.concat(action.pet)}
+    case "REMOVE_PET":
+      return {pets: state.pets.filter(pet => pet.id !== action.id)}
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  const container = document.getElementById("container")
+  state.pets.forEach(pet => container.innerHTML = `<ul><li>${pet.name}</li></ul>`)
 }
