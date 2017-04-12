d439e0d13ce96fba7ad9bfeaab51726e67be2cab
chasms <chasms@chas.ms>
2017-04-12 13:50:48 -0400

    1 pass

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,10 +1,19 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state, action){
+  switch (action.type) {
+    case '@@INIT':
+
+      break;
+    default: return state
+
+  }
 }
 
 export function dispatch(){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
