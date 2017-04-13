0369eaecefc0d164fea5ef92af0e0143dae92d6d
dgwp-projects <zayneabraham1@gmail.com>
2017-04-13 14:24:03 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,30 @@
+import  jsdom from 'jsdom';
+
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets: []}, action){
+
+	switch (action.type) {
+ 
+      case 'ADD_PET':
+        return { pets: [...state.pets, action.pet] }
+ 	  case 'REMOVE_PET':
+ 	  	return { pets: state.pets.filter( p => p.id != action.id )}
+      default:
+        return state;
+    }
 }
 
-export function dispatch(){
+export function dispatch(action){
+	state = managePets(state, action)
+	render()
 }
 
 export function render(){
-
+	global.document = jsdom.jsdom('<html><body><div id="container"></div></body></html>');
+	let thing1 = document.getElementById('container');
+	thing1.innerHTML = '<ul><li>' + state.pets.map( thing => thing.name) + '</li></ul>'
 }
+
+// dispatch({type: "@@INIT"})
